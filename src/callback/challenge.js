5180733c5116f7146c1851986be60b5837ca5d6e
sarmijavier <hjsarmiento1999@gmail.com>
2020-08-05 15:39:19 -0500

    ends of course

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,5 +1,5 @@
 let XMLHtppRequest = require('xmlhttprequest').XMLHttpRequest
-
+let API = 'https://rickandmortyapi.com/api/character/'
 
 function fetchData(url_api, callback) {
     let xhttp = new XMLHtppRequest()
@@ -15,4 +15,18 @@ function fetchData(url_api, callback) {
         }
     }
     xhttp.send()
-}
+}  
+
+
+fetchData(API, function (error1, data1) {
+    if(error1) return console.error(error1)
+    fetchData(API + data1.results[0].id, function (error2, data2) {
+      if(error2) return console.error(error2)
+      fetchData(data2.origin.url, function (error3, data3) {
+          if(error3) return console.log(error3)
+          console.log(data1.info.count)
+          console.log(data2.name)
+          console.log(data3.dimension)
+        })  
+    })
+})
---
+++ src/promises/index.js
@@ -0,0 +1,42 @@
+const somethingWillHappen = () => {
+    return new Promise((resolve, reject) => {
+        if (true) {
+            resolve('Hey!')
+        } else {
+            reject('Whoooops!')
+        }
+    })
+}
+
+somethingWillHappen()
+    .then( response => console.log(response))
+    .catch(err => console.error(err))
+
+
+
+const sometimesWillHappen2 = () => {
+    return new Promise((resolve, reject) => {
+        if(true) {
+            setTimeout(() => {
+                resolve('True')
+            },2000)
+        } else {
+            const error = new Error('Whooop')
+            reject(error)
+        }
+    })
+}
+
+sometimesWillHappen2()
+    .then(response => console.log(response))
+    .catch(err  => console.error(err))
+
+
+
+Promise.all([somethingWillHappen(), sometimesWillHappen2()])
+    .then(response => {
+        console.log('Array of results', response)
+    })
+    .catch(err => {
+        console.log(err)
+    })
